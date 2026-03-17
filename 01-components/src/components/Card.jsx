52833988c8a922819667b619582dd3acaf769785
ashutosh-980 <ashutoshmishra110803@gmail.com>
2026-03-17 11:12:07 +0530

    Components in React

---
+++ 01-components/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Harry</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
+        provident cumque, voluptatum illo rerum vero in id praesentium fugiat
+        debitis deserunt dolorem mollitia veniam voluptatibus?
+      </p>
+    </div>
+  );
+};
+
+export default Card;
