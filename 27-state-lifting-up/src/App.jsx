842f02ed49fbef4d842a847b1e58644f875b6498
ashutosh-980 <ashutoshmishra110803@gmail.com>
2026-09-12 16:53:10 +0530

    Learns about State Lifting Up

---
+++ 27-state-lifting-up/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Card from "./components/Card";
+
+const App = () => {
+  // For State Lifting Up :- Parent Component should do these things 
+  // (1) Create State
+  // (2) Manage State 
+  // (3) Change State
+  // (4) Sabhi Child mein State ko sync krwadunga
+  const [name, setName] = useState("");
+  return (
+    <div id="container">
+      {/* // <Card name="Alice" age={24} /> */}
+      <Card title="Card-1" name={name} setName={setName} />
+      <Card title="Card-2" name={name} setName={setName} />
+      <p style={{color: "white"}}>Inside the Parent Component :- {name} </p>
+    </div>
+  );
+};
+
+export default App;
---
+++ 27-state-lifting-up/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+      <div id="innerContainer">
+        <input
+          type="text"
+          placeholder="Enter any text here..."
+          value={props.name}
+          onChange={(e) => props.setName(e.target.value)}
+        />
+        <p>The value of Input Text inside {props.title} child component is :- {props.name}</p>
+      </div>
+  );
+};
+
+export default Card;
