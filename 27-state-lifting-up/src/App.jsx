352ecb2c95cb2e73c4121f4b921c09a161153b18
ashutosh-980 <ashutoshmishra110803@gmail.com>
2026-09-12 16:55:18 +0530

    Modified the component

--- 27-state-lifting-up/src/App.jsx
+++ 27-state-lifting-up/src/App.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Card from "./components/Card";
 
 const App = () => {
-  // For State Lifting Up :- Parent Component should do these things 
+  // For State Lifting Up :- Parent Component should do these things
   // (1) Create State
-  // (2) Manage State 
+  // (2) Manage State
   // (3) Change State
   // (4) Sabhi Child mein State ko sync krwadunga
   const [name, setName] = useState("");
@@ -12,7 +12,7 @@ const App = () => {
     <div id="container">
       <Card title="Card-1" name={name} setName={setName} />
       <Card title="Card-2" name={name} setName={setName} />
-      <p style={{color: "white"}}>Inside the Parent Component :- {name} </p>
+      <p style={{ color: "white" }}>Inside the Parent Component :- {name} </p>
     </div>
   );
 };
--- 27-state-lifting-up/src/components/Card.jsx
+++ 27-state-lifting-up/src/components/Card.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 const Card = (props) => {
   return (
-      <div id="innerContainer">
-        <input
-          type="text"
-          placeholder="Enter any text here..."
-          value={props.name}
-          onChange={(e) => props.setName(e.target.value)}
-        />
-        <p>The value of Input Text inside {props.title} child component is :- {props.name}</p>
-      </div>
+    <div id="innerContainer">
+      <input
+        type="text"
+        placeholder="Enter any text here..."
+        value={props.name}
+        onChange={(e) => props.setName(e.target.value)}
+      />
+      <p>
+        The value of Input Text inside {props.title} child component is :-{" "}
+        {props.name}
+      </p>
+    </div>
   );
 };
 
